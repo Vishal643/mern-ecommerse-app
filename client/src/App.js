b9afd3bdc60a98f87be2620b9d1a4549ded47579
vishal643 <vishalkumar199812@gmail.com>
2021-04-23 17:36:22 +0530

    done with some basic front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-function App() {
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
+import { Container } from 'react-bootstrap';
+
+const App = () => {
 	return (
-		<div className='App'>
-			<h1>Hello World</h1>
-		</div>
+		<Router>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<Route path='/' component={HomeScreen} exact />
+					<Route path='/product/:id' component={ProductScreen} exact />
+				</Container>
+			</main>
+			<Footer />
+		</Router>
 	);
-}
+};
 
 export default App;
