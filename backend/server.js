f0b91f917fbbcd3b5d3737330252b9cdb0a7ce82
vishal643 <vishalkumar199812@gmail.com>
2021-04-29 09:21:24 +0530

    done with mongodb and models

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import products from './data/product.js';
 import connectDB from './config/db.js';
+import colors from 'colors';
 
+import productRoutes from './routes/ProductsRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+//middlewares
 dotenv.config();
 const app = express();
 
+//database
 connectDB();
 
-app.get('/', (req, res) => {
-	res.send('API is running...');
-});
-app.get('/api/products', (req, res) => {
-	res.json(products);
-});
-app.get('/api/products/:id', (req, res) => {
-	const product = products.find((p) => p._id === req.params.id);
-	res.json(product);
-});
+//routes
+app.use('/api/products', productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
+//Server starting
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, console.log(`Server is running on port ${PORT}`.yellow.bold));
